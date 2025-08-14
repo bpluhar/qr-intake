f4165708703094a03ba659d363be4c593f4611dd
Brian Pluhar <pluharbrian@gmail.com>
2025-08-14 13:20:27 -0400

    Fixed unused vars

--- app/dashboard/SidebarClient.tsx
+++ app/dashboard/SidebarClient.tsx
@@ -318,7 +318,7 @@ function IconTesting(props: SVGProps<SVGSVGElement>) {
 
 function IconSignout(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props} className={["size-4", props.className].filter(Boolean).join(" ")}>
       <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
     </svg>
   );
